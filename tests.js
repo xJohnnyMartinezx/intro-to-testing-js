f6cbc078efa31569a79e9c75a72f16ec15fefe2a
Johnny Martinez <mrtnz.jn@gmail.com>
2022-02-08 15:05:38 -0600

    modified code.js and tests.js.

--- tests.js
+++ tests.js
@@ -17,6 +17,9 @@ describe('helloWorld', function() {
 describe("sayHello", function(){
     it("should be a defined function", function() {
         expect(typeof sayHello).toBe("function");
-    })
+    });
+    it("should return a string when called", function (){
+        expect(typeof sayHello()).toBe("string");
+        });
 
 });
